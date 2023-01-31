f94a90fd408f7f5f519aa67c2c5e274ac042df4d
daitran2k1 <tranquangdai7a@gmail.com>
2023-02-01 02:01:43 +0700

    initialize phobert model

---
+++ run.sh
@@ -0,0 +1,20 @@
+export lr=3e-5
+export s=100
+echo "${lr}"
+export MODEL_DIR=phobert-crf
+export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
+echo "${MODEL_DIR}"
+python3 main.py --token_level word \
+                  --model_type phobert \
+                  --model_dir $MODEL_DIR \
+                  --data_dir PHONER_COVID19 \
+                  --seed $s \
+                  --do_train \
+                  --do_eval \
+                  --save_steps 140 \
+                  --logging_steps 140 \
+                  --num_train_epochs 50 \
+                  --tuning_metric slot_f1 \
+                  --use_crf \
+                  --gpu_id 0 \
+                  --learning_rate $lr
